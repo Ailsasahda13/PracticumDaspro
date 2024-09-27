2ab0e4450710cb3092ceb2a3086607ad9d77a7b1
Ailsasahda13 <ailsashda13@gmail.com>
2024-09-27 07:49:54 +0700

    first commit

---
+++ PemilihanHari03.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+/**
+ * PemilihanHari03
+ */
+public class PemilihanHari03 {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner (System.in);
+       
+    }
+}
